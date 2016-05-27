80c17addd40797b5f08b1e1280b6e8781859f588
Julio Fernández <jfernandezcorral@gmail.com>
2016-05-27 20:09:06 +0200

    mas

--- a.js
+++ a.js
@@ -21,3 +21,39 @@ var Character = (function () {
     };
     return Character;
 }());
+function add(foo, bar, x) {
+    if (x === void 0) { x = 0; }
+    return foo + bar + x;
+}
+var v = add(7, 4);
+console.log(v);
+function add2() {
+    var foo = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        foo[_i - 0] = arguments[_i];
+    }
+    var result = 0;
+    for (var i = 0; i < foo.length; i++) {
+        result += foo[i];
+    }
+    return result;
+}
+console.log(add2(37, 24, 5));
+function test(value) {
+    switch (typeof value) {
+        case "string":
+            return "My\tname\tis\t" + value + ".";
+        case "number":
+            return "I'm\t" + value + "\tyears\told.";
+        case "boolean":
+            return value ? "I'm	single." : "I'm	not	single.";
+        default:
+            console.log("Invalid	Operation!");
+    }
+}
+test(45);
+function hola(v) {
+    console.log('hola');
+}
+var a = window;
+var pp;
--- a.ts
+++ a.ts
@@ -15,3 +15,39 @@ class Character{
 	}
 }
 
+function add(foo: number, bar: number, x: number = 0): number{
+	return foo + bar + x;
+}
+
+var v = add(7, 4);
+console.log(v);
+function add2(...foo: number[]): number{
+    var result = 0;
+    for (var i = 0; i < foo.length; i++) {
+        result += foo[i];
+    }
+    return result;
+}
+console.log(add2(37, 24, 5));
+function test(name: string): string;				//	overloaded	signature
+function test(age: number): string;					//	overloaded	signature
+function test(single: boolean): string;	//	overloaded	signature
+function test(value: (string | number | boolean))	:	string	{	//implementation	signature
+		switch(typeof value){
+			case	"string":
+				return	`My	name	is	${value}.`;
+			case	"number":
+				return	`I'm	${value}	years	old.`;	
+			case	"boolean":
+				return	value	?	"I'm	single.":	"I'm	not	single.";	
+			default:
+				console.log("Invalid	Operation!");
+		}
+}
+
+test(45);
+function hola(v) {
+    console.log('hola');
+}
+var a = window;
+var pp;
